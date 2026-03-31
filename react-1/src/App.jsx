9dc05533e809474988df01c4b94ef6c36b10aa3c
Ansh Chatare <ansh.chatare.228@gmail.com>
2026-03-31 16:03:44 +0530

    created first react file through vite

---
+++ react-1/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <body className='bg-black'>
+        <nav className='bg-gray-500 flex items-center justify-around h-[5rem]'>
+          <h1 className='text-green-900 text-2xl'>Hello Man</h1>
+          <h1 className='text-green-900 text-2xl'>Hello Man</h1>
+          <h1 className='text-green-900 text-2xl'>Hello Man</h1>
+          <h1 className='text-green-900 text-2xl'>Hello Man</h1>
+          <h1 className='text-green-900 text-2xl'>Hello Man</h1>
+        </nav>
+      </body>
+    </>
+  )
+}
+
+export default App
